e9144fe2cccfc5730d7fb73725a7e8b6fc11d87f
jee14 <3aroa2@gmail.com>
2023-11-10 11:19:51 +0900

    feature: Distributor test fixture 추가 [#5]

---
+++ src/test/java/com/metime/alcohol/fixture/distributor/DistributorFixture.java
@@ -0,0 +1,9 @@
+package com.metime.alcohol.fixture.distributor;
+
+import com.metime.alcohol.domain.distributor.Distributor;
+
+public class DistributorFixture {
+
+	public static final Distributor CONVENIENCE_STORE = new Distributor("편의점");
+	public static final Distributor SUPERMARKET = new Distributor("대형마트");
+}
