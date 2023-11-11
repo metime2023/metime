e41a7dd5b51d3f43e01de9d90edede1178c1ebcb
youngreal <59333182+youngreal@users.noreply.github.com>
2023-11-12 04:05:56 +0900

    feat: 댓글 작성 - #14
    - 댓글 작성시간이 현재 LocalDateTime.now()로 되어있는데 어떻게 표현할건지 요구사항을 구체적으로 들은후 변경 예정입니다.

---
+++ src/main/java/com/metime/alcohol/service/comment/CommentService.java
@@ -0,0 +1,26 @@
+package com.metime.alcohol.service.comment;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.metime.alcohol.common.exception.DoesNotExistAlcoholException;
+import com.metime.alcohol.domain.Alcohol;
+import com.metime.alcohol.domain.Comment;
+import com.metime.alcohol.repository.AlcoholRepository;
+import com.metime.alcohol.repository.comment.CommentRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class CommentService {
+
+	private final AlcoholRepository alcoholRepository;
+	private final CommentRepository commentRepository;
+
+	public void create(long alcoholId, String content) {
+		Alcohol alcohol = alcoholRepository.findById(alcoholId).orElseThrow(DoesNotExistAlcoholException::new);
+		commentRepository.save(Comment.create(alcohol, content));
+	}
+}
