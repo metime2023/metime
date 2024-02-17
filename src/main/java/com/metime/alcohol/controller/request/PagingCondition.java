2847253940a138ef48b2131a3b49cf2ae335cc4e
jiseunghyeon <3aroa2@gmail.com>
2024-02-17 11:46:58 +0900

    feature: PagingCondition 디폴트 값

--- src/main/java/com/metime/alcohol/controller/request/PagingCondition.java
+++ src/main/java/com/metime/alcohol/controller/request/PagingCondition.java
@@ -7,28 +7,44 @@ import com.metime.alcohol.dto.PagingDto;
 import jakarta.validation.constraints.Min;
 
 public record PagingCondition(
-        long cursorNo,
-        @Min(1)
-        int displayPerPage,
-        @SortCondition(enumClass = AlcoholSort.class, ignoreCase = true)
-        String sort,
-        int minPrice,
-        int maxPrice
+    long cursorNo,
+    @Min(1)
+    int displayPerPage,
+    @SortCondition(enumClass = AlcoholSort.class, ignoreCase = true)
+    String sort,
+    int minPrice,
+    int maxPrice
 ) {
 
-        private static final int DEFAULT_VALUE = 0;
+    private static final int DEFAULT_CURSOR_NO = 0;
+    private static final int DEFAULT_DISPLAY_PER_PAGE = 10;
+    private static final int DEFAULT_MAX_PRICE = 10000;
 
-        public boolean isCursorDefaultValue() {
-                return this.cursorNo <= DEFAULT_VALUE;
-        }
+    public boolean isCursorDefaultValue() {
+        return this.cursorNo <= DEFAULT_CURSOR_NO;
+    }
 
-        public PagingDto toDto() {
-                return PagingDto.builder()
-                    .cursorNo(this.cursorNo)
-                    .displayPerPage(this.displayPerPage)
-                    .sort(this.sort)
-                    .minPrice(this.minPrice)
-                    .maxPrice(this.maxPrice)
-                    .build();
+    public int getDisplayPerPage() {
+        if (this.displayPerPage <= 0) {
+            return DEFAULT_DISPLAY_PER_PAGE;
         }
+        return this.displayPerPage;
+    }
+
+    public int getMaxPrice() {
+        if (this.maxPrice <= 0) {
+            return DEFAULT_MAX_PRICE;
+        }
+        return this.maxPrice;
+    }
+
+    public PagingDto toDto() {
+        return PagingDto.builder()
+            .cursorNo(this.cursorNo)
+            .displayPerPage(displayPerPage)
+            .sort(this.sort)
+            .minPrice(this.minPrice)
+            .maxPrice(getMaxPrice())
+            .build();
+    }
 }
