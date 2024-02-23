85f0aad944cdacd64e2111a7c62aa48ea30511fe
jee14 <3aroa2@gmail.com>
2024-02-23 17:38:23 +0900

    refactor: application 이름 변경

--- src/main/java/com/metime/Application.java
+++ src/main/java/com/metime/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MetimeApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MetimeApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
